46a64de19d91a9324d53c0183f5215d3189258e4
Brandon Stafford <brandon@pingswept.org>
2011-06-30 16:51:22 -0700

    Add explanation of sed expression for finding hostname.

--- recipes/initscripts/files/rascal-hostname.sh
+++ recipes/initscripts/files/rascal-hostname.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Explanation of the sed command below
+# 
+# sed -ne s/thing1/thing2/p /path/to/file means:
+#    "Search the file for thing1, replace it with thing2,
+#    and only print the lines that you changed."
+# [^:]* means zero or more characters from the set "not colons"
+# \(something\) means "save this for later"
+# \1 means "retrieve the first thing you saved"
+#
+# Overall, the command means:
+#    "Search /proc/cmdline for 5 colons,
+#    and print what's between colons 4 and 5."
+
 HOSTNAME="`sed -ne 's/^[^:]*:[^:]*:[^:]*:[^:]*:\([^:]*\):.*/\1/p' /proc/cmdline`"
 
 if [ -z "$HOSTNAME" ]
