a8b550630c8af228e690916831c4e52cb4aec4e4
Brandon Stafford <brandon@pingswept.org>
2012-08-03 23:19:02 -0300

    Edit list to prevent clash between I2C and GPIO

--- recipes/initscripts/files/rascal-gpio.sh
+++ recipes/initscripts/files/rascal-gpio.sh
@@ -1,4 +1,4 @@
-for pin in 55 56 64 65 66 67 68 69 70 71 72 73 96 97 98 99 100 101 107
+for pin in 64 65 66 67 68 69 70 71 72 73 96 97 98 99 100 101 107
 do
     echo $pin > /sys/class/gpio/export
     echo out > /sys/class/gpio/gpio$pin/direction
