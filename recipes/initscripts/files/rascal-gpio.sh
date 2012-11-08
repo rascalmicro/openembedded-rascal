31624af4b7c26db1b010d8a83f13bc35dd271f14
Brandon Stafford <brandon@pingswept.org>
2012-11-08 19:31:37 +0000

    Remove I2C and SPI pins from GPIO list (until pinctrl works)

--- recipes/initscripts/files/rascal-gpio.sh
+++ recipes/initscripts/files/rascal-gpio.sh
@@ -1,4 +1,4 @@
-for pin in 64 65 66 67 68 69 70 71 72 73 96 97 98 99 100 101 107
+for pin in 68 69 70 71 72 73 74 75 96 97 98 107
 do
     echo $pin > /sys/class/gpio/export
     echo out > /sys/class/gpio/gpio$pin/direction
